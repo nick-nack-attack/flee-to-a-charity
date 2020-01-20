4c1fe3cdeca90d590b8dafdeee19abd15ea432d9
Nick Farni <nickfarni001@gmail.com>
2020-01-20 16:48:35 -0700

    Changed function to get new zipcode before executing get charity

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ function displayResults(responseJson, userSubject) {
         <p>located in ${inCity}, ${inState}</p>
         <p>Category: ${ofCategory}</p>
         <p>${ofClassification}</p>
-        <p>Their mission is: ${theirMission}</p>
         <p><a class="learn-more" href="${atUrl}" target="_blank">Learn more</a></p>
         </div>
     `)
@@ -66,7 +65,7 @@ function getFarAwayZipcode(userZipcode) {
 
 function getCharity(userSubject, userZipcode) {
     console.log(` getCharity() is running ... `);
-    
+    getFarAwayZipcode(userZipcode).then(newZipCode => { 
         const charityID = '6bc077c3';
         const charityApiKey = 'f847dcb76780f7b7babb31b6249cdfc3' // '42beb0038b32282e5146064515efe2db';
         const charityApiUrl = 'https://api.data.charitynavigator.org/v2/Organizations' //'http://data.orghunter.com/v1/charitysearch';
@@ -75,7 +74,7 @@ function getCharity(userSubject, userZipcode) {
             app_key: charityApiKey,
             search: userSubject,
             pageSize: 1,
-            zip: getFarAwayZipcode(userZipcode) // getFarAwayZipcode(userZipcode),
+            zip: newZipCode // getFarAwayZipcode(userZipcode),
         }
         const queryString = formatQueryParams(params);
         const url = charityApiUrl + "?" + queryString;
@@ -91,7 +90,7 @@ function getCharity(userSubject, userZipcode) {
             .catch(error => {
                 $('#js-error-message').removeClass('hidden').text('Something went wrong: ' + error.message)
             });
-    
+        })
 
 }
 
