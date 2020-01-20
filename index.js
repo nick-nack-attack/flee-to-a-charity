004cbaaa2391bf585d039d553503e4927a41ba87
Nick Farni <nickfarni001@gmail.com>
2020-01-20 14:45:59 -0700

    fetching charity data from new api

--- index.js
+++ index.js
@@ -8,19 +8,26 @@ function initializePage() {
     `)
 }
 
-function displayResults(responseJson) {
+function displayResults(responseJson, userSubject) {
     console.log(` displayResults(${responseJson}) is running ... `);
+    const theCharity = JSON.stringify(responseJson[0].charityName);
+    const theirMission = JSON.stringify(responseJson[0].mission);
+    const inCity = JSON.stringify(responseJson[0].mailingAddress.city);
+    const inState = JSON.stringify(responseJson[0].mailingAddress.stateOrProvince);
+    const ofCategory = JSON.stringify(responseJson[0].irsClassification.nteeType);
+    const ofClassification = JSON.stringify(responseJson[0].irsClassification.classification);
+    const atUrl = JSON.stringify(responseJson[0].charityNavigatorURL);
+    console.log(theCharity, inCity, ofCategory, atUrl);
     $('#main-area').empty();
     $('#main-area').html(`
-        <div class="card">
-            <h2>Entered Topic</h2>
-        </div>
-        <div class="card">
-            <h3>Charity Name</h3>
-            <p>City</p>
-            <p>Distance Away</p>
-            <p>Description</p>
-            <button>Learn more</button>
+        <div>
+        <p>Your ${userSubject} charity is ...</p>
+        <h2>${theCharity}</h2>
+        <p>located in ${inCity}, ${inState}</p>
+        <p>Category: ${ofCategory}</p>
+        <p>${ofClassification}</p>
+        <p>Their mission is: ${theirMission}</p>
+        <p><a class="learn-more" href="${atUrl}" target="_blank">Learn more</a></p>
         </div>
     `)
 }
@@ -54,55 +61,34 @@ function getDistance(userZipcode) {
         });
 }
 
-function createCORSRequest(method, url) {
-    let xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-        xhr.open(method, url, true);
-    }
-    else if (typeof XDomainRequest != "undefined") {
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    }
-    else {
-        xhr = null;
-    }
-    return xhr;
-}
-
-// let xhr = createCORSRequest('GET', url);
-// if (!xhr) {
-//    throw new Error('CORS not supported');
-// }
-
-function getTitle(text) {
-    return text.match('<title>(.*)?</title>')
-}
-
 */
 
-function getCharity(userSubject) {
+function getCharity(userSubject, userZipcode) {
     console.log(` getCharity(${userSubject}) is running ... `);
-    const charityApiKey = '42beb0038b32282e5146064515efe2db';
-    const charityApiUrl = 'http://data.orghunter.com/v1/charitysearch';
+    const charityID = '6bc077c3';
+    const charityApiKey = 'f847dcb76780f7b7babb31b6249cdfc3' // '42beb0038b32282e5146064515efe2db';
+    const charityApiUrl = 'https://api.data.charitynavigator.org/v2/Organizations' //'http://data.orghunter.com/v1/charitysearch';
     const params = {
-        user_key: charityApiKey,
-        searchTerm: userSubject,
+        app_id: charityID,
+        app_key: charityApiKey,
+        search: userSubject,
+        pageSize: 1,
+        zip: userZipcode,
         // zipCode: userZipcode,
-        city: 'memphis',
-        rows: 1
         // distance: getDistance(userZipcode)
     }
     const queryString = formatQueryParams(params);
     const url = charityApiUrl + "?" + queryString;
-    console.log(`This is the url: ${url}`);
-    fetch(url, {mode: 'cors'})
+    const throughServerUrl = `https://cors-anywhere.herokuapp.com/${url}`;
+    console.log(throughServerUrl);
+    fetch(throughServerUrl)
         .then(response => {
             if (response.ok) {
                 return response.json();
             }
             throw new Error(response.statusText);
         })
-        .then(responseJson => displayResults(responseJson))
+        .then(responseJson => displayResults(responseJson, userSubject))
         .catch(error => {
             $('#js-error-message').removeClass('hidden').text('Something went wrong: ' + error.message)
         });
@@ -115,7 +101,7 @@ function watchForm() {
         event.preventDefault();
         const userSubject = $('#subject-you-search-for').val();
         const userZipcode = $('#zipcode-you-live-in').val();
-        getCharity(userSubject);
+        getCharity(userSubject, userZipcode);
     })
 }
 
